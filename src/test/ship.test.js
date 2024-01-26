b6a3ccfa45bbebaaedeeb7d77a85fac8c5a72760
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-01-26 09:48:26 +0800

    add test cases for Ship class

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -1,7 +1,7 @@
 import { Ship } from "../class/ship";
 
 describe("ship length", () => {
-  test("test ship with length 0", () => {
+  test("ship with length 0", () => {
     const ship = new Ship(0);
     expect(ship.length === 0);
   });
@@ -17,24 +17,69 @@ describe("ship length", () => {
   });
 });
 
-describe("hit() method", () => {
-  test("ship hit 0 time", () => {
-    const ship = new Ship(5);
+describe("hit() method on ship of length 5", () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = new Ship(5);
+  });
+
+  test("ship not hit", () => {
+    // const ship = new Ship(5);
     expect(ship.hitCount).toBe(0);
   });
 
   test("ship hit 1 time", () => {
-    const ship = new Ship(5);
     ship.hit();
     expect(ship.hitCount).toBe(1);
   });
 
-  test("ship hit <length - 1 times", () => {
-    const ship = new Ship(5);
+  test("ship hit <length - 1> times", () => {
     ship.hit();
     ship.hit();
     ship.hit();
     ship.hit();
     expect(ship.hitCount).toBe(4);
+  });
+
+  test("ship hit <length> times", () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.hitCount).toBe(5);
+  });
+});
+
+describe("isSunk() on ship of length 4", () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = new Ship(4);
+  });
+
+  test("ship not hit", () => {
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
+  test("ship hit 1 time", () => {
+    ship.hit();
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
+  test("ship hit 3 times", () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
+  test("ship hit <length> times", () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBeTruthy();
   });
 });
