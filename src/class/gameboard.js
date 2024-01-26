3a6d6bcc7cdae5df60506b8422cc9ff8c44b4e12
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-01-26 14:04:44 +0800

    create Gameboard class

---
+++ src/class/gameboard.js
@@ -0,0 +1,25 @@
+class Gameboard {
+  constructor(rows, columns) {
+    this.rows = rows;
+    this.columns = columns;
+    this.board = this.#createBoard(rows, columns);
+  }
+
+  #createBoard(rows, columns) {
+    const board = [];
+    for (let i = 0; i < rows; i++) {
+      const row = [];
+      for (let j = 0; j < columns; j++) {
+        row.push({
+          isShip: false,
+          isHit: false,
+        });
+      }
+      board.push(row);
+    }
+
+    return board;
+  }
+}
+
+export { Gameboard };
