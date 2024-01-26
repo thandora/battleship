03c9533ef97f667ce78c4410182aa7e1163d8ff4
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-01-26 16:11:49 +0800

    add ship property to cells of Gameboard

--- src/class/gameboard.js
+++ src/class/gameboard.js
@@ -1,3 +1,5 @@
+import { Ship } from "./ship";
+
 class Gameboard {
   constructor(rows, columns) {
     this.rows = rows;
@@ -12,6 +14,7 @@ class Gameboard {
       for (let j = 0; j < columns; j++) {
         row.push({
           isShip: false,
+          ship: null,
           isHit: false,
         });
       }
