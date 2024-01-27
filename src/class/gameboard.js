55a8e458dedbced46ec304eeaefb3a6aeb81f4ea
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-01-27 09:35:49 +0800

    add placeship() method and corresponding tests

--- src/class/gameboard.js
+++ src/class/gameboard.js
@@ -23,6 +23,92 @@ class Gameboard {
 
     return board;
   }
+
+  placeShip(length, orientation, coordinates) {
+    // Orientation is horizontal or vertical
+    // 0 = horizontal
+    // 1 = vertical
+
+    if (this.#isValidPlacement(length, orientation, coordinates)) {
+      const ship = new Ship(length);
+      // Row and column start
+      const rStart = coordinates[0];
+      const cStart = coordinates[1];
+
+      if (orientation === 0) {
+        for (let i = cStart; i < cStart + length; i++) {
+          this.board[rStart][i] = { isShip: true, ship: ship, isHit: false };
+        }
+      } else {
+        for (let i = rStart; i < rStart + length; i++) {
+          this.board[i][cStart] = { isShip: true, ship: ship, isHit: false };
+        }
+      }
+
+      // Successfully placed.
+      return true;
+    }
+
+    // Failed to place.
+    return false;
+  }
+
+  #isValidPlacement(length, orientation, coordinates) {
+    // Row and column start
+    const xStart = coordinates[0];
+    const yStart = coordinates[1];
+
+    // Orientation = 0 (horizontal)
+    if (orientation === 0) {
+      for (let i = xStart; i < xStart + length; i++) {
+        // Out of bounds check
+        if (!this.#isInBound(i, yStart)) {
+          return false;
+        }
+
+        // Check if ship already occupying space
+        if (this.board[xStart][i].isShip) {
+          return false
+        }
+      }
+    }
+
+    // Orientation = 1 (vertical)
+    else {
+      for (let i = yStart; i < yStart + length; i++) {
+        // Out of bounds check
+        if (!this.#isInBound(xStart, i)) {
+          return false;
+        }
+
+        // Check if ship already occupying space
+        if (this.board[i][yStart].isShip) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  #isInBound(x, y) {
+    const xCoordMax = this.board[0].length - 1;
+    const yCoordMax = this.board.length - 1;
+
+    if (x >= xCoordMax) {
+      return false;
+    } else if (x < 0) {
+      return false;
+    }
+
+    if (y >= yCoordMax) {
+      return false;
+    } else if (y < 0) {
+      return false;
+    }
+
+    return true;
+  }
 }
 
 export { Gameboard };
--- src/test/gameboard.test.js
+++ src/test/gameboard.test.js
@@ -1,27 +1,100 @@
 import { Gameboard } from "../class/gameboard";
+import { Ship } from "../class/ship";
 
+// Test for Gameboard.board validity
 describe("board (rows x columns)", () => {
-  const cell = { isShip: false, isHit: false };
+  const cell = { ship: null, isShip: false, isHit: false };
 
   test("0 x 0 board", () => {
     const gameboard = new Gameboard(0, 0);
-    expect(gameboard.board).toEqual([]);
+    const testBoard = [];
+
+    expect(gameboard.board).toEqual(testBoard);
   });
 
   test("1 x 1 board", () => {
     const gameboard = new Gameboard(1, 1);
-    const mockBoard = [[cell]];
+    const testBoard = [[cell]];
 
-    expect(gameboard.board).toEqual(mockBoard);
+    expect(gameboard.board).toEqual(testBoard);
   });
 
   test("2 x 3 board", () => {
     const gameboard = new Gameboard(2, 3);
-    const mockBoard = [
+    const testBoard = [
       [cell, cell, cell],
       [cell, cell, cell],
     ];
 
-    expect(gameboard.board).toEqual(mockBoard);
+    expect(gameboard.board).toEqual(testBoard);
+  });
+});
+
+// Test for Gameboard.placeShip() on valid placements
+describe("place ship at valid positions in 10 x 10 board. ", () => {
+  let gameboard;
+  let testBoard;
+
+  beforeEach(() => {
+    testBoard = new Gameboard(10, 10).board;
+    gameboard = new Gameboard(10, 10);
+  });
+
+  // Coordinates state the starting cell of the ship.
+  test("horizontal ship of length 4 at (0, 0)", () => {
+    const ship = new Ship(4);
+    gameboard.placeShip(4, 0, [0, 0]);
+
+    for (let i = 0; i < 4; i++) {
+      testBoard[0][i] = { isShip: true, ship: ship, isHit: false };
+    }
+
+    expect(gameboard.board).toEqual(testBoard);
+  });
+
+  test("vertical ship of length 4 at (0, 0)", () => {
+    const ship = new Ship(4);
+    gameboard.placeShip(4, 1, [0, 0]);
+
+    for (let i = 0; i < 4; i++) {
+      testBoard[i][0] = { isShip: true, ship: ship, isHit: false };
+    }
+
+    expect(gameboard.board).toEqual(testBoard);
+  });
+
+  test("horizontal ship of length 4 at (3, 5)", () => {
+    const ship = new Ship(4);
+    gameboard.placeShip(4, 0, [3, 5]);
+
+    for (let i = 5; i < 9; i++) {
+      testBoard[3][i] = { isShip: true, ship: ship, isHit: false };
+    }
+    expect(gameboard.board).toEqual(testBoard);
+  });
+});
+
+// Test for Gameboard.placeShip() on invalid placements
+describe("place ships at an invalid positions in 10 x 10 board.", () => {
+  let gameboard;
+
+  beforeEach(() => {
+    gameboard = new Gameboard(10, 10);
+  });
+
+  test("horizontal ship of length 4 at (8, 0)", () => {
+    expect(gameboard.placeShip(4, 0, [8, 0])).toBeFalsy();
+  });
+
+  test("vertical ship of length 4 at (3, 9)", () => {
+    expect(gameboard.placeShip(4, 1, [3, 9])).toBeFalsy();
+  });
+
+  test("placing ship of length 2 on already occupied space", () => {
+    const cell = { ship: null, isShip: true, isHit: false };
+    gameboard.board[0][0] = cell;
+    gameboard.board[0][1] = cell;
+
+    expect(gameboard.placeShip(2, 0, [0, 0])).toBeFalsy();
   });
 });
