a5f44745d8cf8032d7d68f55f4229bb925534f2f
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-01-30 15:07:01 +0800

    createBoard() now takes input from DOM

--- src/dom.js
+++ src/dom.js
@@ -1,9 +1,23 @@
-function createBoard(rows, columns, boardContainer) {
-  for (let row = 0; row < rows; row++) {
+function createBoard(gameBoard, boardContainer) {
+  const nRows = gameBoard.board.length;
+  const nColumns = gameBoard.board[0].length;
+
+  for (let row = 0; row < nRows; row++) {
     const rowCells = document.createElement("div");
-    for (let column = 0; column < columns; column++) {
+
+    for (let column = 0; column < nColumns; column++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
+
+      cell.addEventListener("click", () => {
+        if (gameBoard.receiveAttack([row, column])) {
+          cell.classList.add("hit");
+
+          if (gameBoard.board[row][column].isShip) {
+            cell.classList.add("ship-hit");
+          }
+        }
+      });
       rowCells.appendChild(cell);
     }
     boardContainer.appendChild(rowCells);
