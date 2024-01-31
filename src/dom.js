b7dff20f5de546d3eef55b34a4d543203a7adfca
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-01-31 13:25:36 +0800

    refactor board render logic
    Board rendering is now disconnect from the actual action of attacking a
    board cell.

--- src/dom.js
+++ src/dom.js
@@ -4,33 +4,45 @@ class Dom {
 
     for (const i in players) {
       const boardNode = boardNodes[i];
-      const nRows = players[i].gameBoard.board.length;
-      const nColumns = players[i].gameBoard.board[0].length;
-      this.createBoard([nRows, nColumns], boardNode);
+      this.clearBoard(boardNode);
+
+      const cPlayer = players[i];
+      const nRows = cPlayer.gameBoard.board.length;
+      const nColumns = cPlayer.gameBoard.board[0].length;
+
+      this.createBoard([nRows, nColumns], boardNode, cPlayer.gameBoard.board);
     }
   }
 
-  createBoard([rows, columns], boardNode) {
+  createBoard([rows, columns], boardNode, board) {
     for (let row = 0; row < rows; row++) {
       const rowCells = document.createElement("div");
 
       for (let column = 0; column < columns; column++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
+        const cellNode = document.createElement("div");
+        const cell = board[row][column];
+        cellNode.classList.add("cell");
 
-        rowCells.appendChild(cell);
+        this.assignHitClass(cellNode, cell);
+        rowCells.appendChild(cellNode);
       }
       boardNode.appendChild(rowCells);
     }
   }
 
-  assignHitClass([row, column], cell, gameBoard) {
-    if (gameBoard.receiveAttack([row, column])) {
-      cell.classList.add("hit");
+  assignHitClass(cellNode, cell) {
+    if (cell.isHit) {
+      cellNode.classList.add("hit");
 
-      if (gameBoard.board[row][column].isShip) {
-        cell.classList.add("ship-hit");
+      if (cell.isShip) {
+        cellNode.classList.add("ship-hit");
       }
+    }
+  }
+
+  clearBoard(boardNode) {
+    while (boardNode.firstChild) {
+      boardNode.removeChild(boardNode.firstChild);
     }
   }
 }
